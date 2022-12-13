0f3614226e63c7752685a3583c04a72718dc9efa
spitkane19 <55877672+spitkane19@users.noreply.github.com>
2022-12-13 10:34:02 +0200

    Add files via upload
    Arduino data now seen in the website

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+require('dotenv').config()
+
+console.log(process.env.API_KEY);
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+var app = express();
+
+var server = app.listen(4000, () => { //Start the server, listening on port 4000.
+    console.log("Listening to requests on port 4000...");
+})
+
+var port = new SerialPort('COM3',{ 
+    baudRate: 115200,
+    dataBits: 8,
+
+});
+
+port.pipe(parser);
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: 'public'});
+})
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    
+    console.log('Node is listening to port');
+    
+});
+
+parser.on('data', function(data) {
+    
+    console.log(data);
+    
+    io.emit('data', data);
+    
+});
+ //Bind socket.io to our express server.
+
+app.use(express.static('public')); //Send index.html page on GET /
