824b345cddc5ecacfc317a743caf7a8389fc4812
eemelihayrynen <55876843+eemelihayrynen@users.noreply.github.com>
2022-12-21 15:05:25 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -34,11 +34,11 @@ io.on('connection', function(socket) {
 });
 
 parser.on('data', function(data) {
-    temp = String(data).split(","); //formatted the data so it only has the numbers of the temperature
-    temp = temp[1].split("").splice(2 - 1 , 4);
-    temp = Number(temp.join(""));
-    console.log(temp);
-    io.emit('data', temp);
+    temps = String(data).split(",");    //Got rid of the humidity
+    temps = temps[1].split("").splice(2 - 1 , 6);
+    temps = temps.join("");
+    console.log(temps);
+    io.emit('data', temps);
 });
  //Bind socket.io to our express server.
 
